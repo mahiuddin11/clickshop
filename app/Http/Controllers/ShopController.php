3b6fdacc662325a4d01f6828340392707fd8cab7
mahiuddin11 <mahiuddinsamad@gmail.com>
2024-12-24 18:13:14 +0600

    shop controller

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function index(){
-        return view('website.shop');
+        $product = Product::orderBy('created_at','desc');
+        return view('website.shop',get_defined_vars());
     }
 }
