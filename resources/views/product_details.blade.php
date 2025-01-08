33742816e3bd27decabfc08524415e8e4f0c3362
mahiuddin11 <mahiuddinsamad@gmail.com>
2025-01-08 12:24:50 +0600

    product detailes

--- resources/views/product_details.blade.php
+++ resources/views/product_details.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('contant')
-    <div class="pt-90">
+    <main class="pt-90">
         <div class="mb-md-1 pb-md-3"></div>
         <section class="product-single container">
             <div class="row">
@@ -11,11 +11,9 @@
                             <div class="swiper-container">
                                 <div class="swiper-wrapper">
                                     <div class="swiper-slide product-single__image-item">
-                                        <img loading="lazy" class="h-auto"
-                                            src="{{ asset('uploads/products/thumbmails/' . $product->image) }}"
+                                        <img loading="lazy" class="h-auto" src="{{ asset('uploads/products/thumbmails') }}/{{ $product->image }}"
                                             width="674" height="674" alt="" />
-                                        <a data-fancybox="gallery"
-                                            href="{{ asset('uploads/products/thumbmails/' . $product->image) }}"
+                                        <a data-fancybox="gallery" href="{{ asset('uploads/products/thumbmails') }}/{{ $product->image }}"
                                             data-bs-toggle="tooltip" data-bs-placement="left" title="Zoom">
                                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
                                                 xmlns="http://www.w3.org/2000/svg">
@@ -23,26 +21,40 @@
                                             </svg>
                                         </a>
                                     </div>
-
-                                    @foreach (explode(',', $product->images) as $gimage)
-                                        <div class="swiper-slide product-single__image-item">
-                                            <img loading="lazy" class="h-auto"
-                                                src="{{ asset('uploads/products/thumbmails/' . $gimage) }}" width="674"
-                                                height="674" alt="" />
-                                            <a data-fancybox="gallery"
-                                                href="{{ asset('uploads/products/thumbmails/' . $gimage) }}"
-                                                data-bs-toggle="tooltip" data-bs-placement="left" title="Zoom">
-                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg">
-                                                    <use href="#icon_zoom" />
-                                                </svg>
-                                            </a>
-                                        </div>
-                                    @endforeach
-
+                                    <div class="swiper-slide product-single__image-item">
+                                        <img loading="lazy" class="h-auto" src="assets/images/products/product_0-1.jpg"
+                                            width="674" height="674" alt="" />
+                                        <a data-fancybox="gallery" href="../images/products/product_0-1.html"
+                                            data-bs-toggle="tooltip" data-bs-placement="left" title="Zoom">
+                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <use href="#icon_zoom" />
+                                            </svg>
+                                        </a>
+                                    </div>
+                                    <div class="swiper-slide product-single__image-item">
+                                        <img loading="lazy" class="h-auto" src="assets/images/products/product_0-2.jpg"
+                                            width="674" height="674" alt="" />
+                                        <a data-fancybox="gallery" href="../images/products/product_0-2.html"
+                                            data-bs-toggle="tooltip" data-bs-placement="left" title="Zoom">
+                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <use href="#icon_zoom" />
+                                            </svg>
+                                        </a>
+                                    </div>
+                                    <div class="swiper-slide product-single__image-item">
+                                        <img loading="lazy" class="h-auto" src="assets/images/products/product_0-3.jpg"
+                                            width="674" height="674" alt="" />
+                                        <a data-fancybox="gallery" href="../images/products/product_0-3.html"
+                                            data-bs-toggle="tooltip" data-bs-placement="left" title="Zoom">
+                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <use href="#icon_zoom" />
+                                            </svg>
+                                        </a>
+                                    </div>
                                 </div>
-
-
                                 <div class="swiper-button-prev"><svg width="7" height="11" viewBox="0 0 7 11"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <use href="#icon_prev_sm" />
@@ -57,17 +69,17 @@
                             <div class="swiper-container">
                                 <div class="swiper-wrapper">
                                     <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto"
-                                            src="{{ asset('uploads/products/thumbmails/' . $product->image) }}" width="104" height="104"
+                                            src="assets/images/products/product_0.jpg" width="104" height="104"
                                             alt="" /></div>
-
-                                            @foreach (explode(',',$product->images) as $gimge )
-                                            <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto"
-                                                src="{{ asset('uploads/products/thumbmails/' . $gimge) }}" width="104" height="104"
-                                                alt="" /></div>
-                                            @endforeach
-
-
-
+                                    <div class="swiper-slide product-single__image-item"><img loading="lazy"
+                                            class="h-auto" src="assets/images/products/product_0-1.jpg" width="104"
+                                            height="104" alt="" /></div>
+                                    <div class="swiper-slide product-single__image-item"><img loading="lazy"
+                                            class="h-auto" src="assets/images/products/product_0-2.jpg" width="104"
+                                            height="104" alt="" /></div>
+                                    <div class="swiper-slide product-single__image-item"><img loading="lazy"
+                                            class="h-auto" src="assets/images/products/product_0-3.jpg" width="104"
+                                            height="104" alt="" /></div>
                                 </div>
                             </div>
                         </div>
@@ -117,15 +129,17 @@
                     </div>
                     <div class="product-single__price">
                         <span class="current-price">
-                            @if ($product->sale_price > 0)
-                                <s>{{ $product->regular_price }}</s> {{ $product->sale_price }}
-                            @else
-                                {{ $product->regular_price }}
+
+                            @if ($product->sale_price)
+                                <s>${{ number_format($product->regular_price, 0) }}</s>
+                                ${{ number_format($product->sale_price, 0) }}
+                            @elseif($product->sale_price == 0)
+                                ${{ number_format($product->regular_price, 0) }}
                             @endif
                         </span>
                     </div>
                     <div class="product-single__short-desc">
-                        <p>{{ $product->short_description }}</p>
+                        <p>{{$product->short_description}}</p>
                     </div>
                     <form name="addtocart-form" method="post">
                         <div class="product-single__addtocart">
@@ -183,15 +197,15 @@
                     <div class="product-single__meta-info">
                         <div class="meta-item">
                             <label>SKU:</label>
-                            <span>{{ $product->SKU }}</span>
+                            <span>{{$product->SKU}}</span>
                         </div>
                         <div class="meta-item">
                             <label>Categories:</label>
-                            <span>{{ $product->Catagory->name }}</span>
+                            <span>{{$product->catagory->name}}</span>
                         </div>
                         <div class="meta-item">
                             <label>Tags:</label>
-                            <span>{{ 'na' }}</span>
+                            <span>{{"N/A"}}</span>
                         </div>
                     </div>
                 </div>
@@ -201,7 +215,7 @@
                     <li class="nav-item" role="presentation">
                         <a class="nav-link nav-link_underscore active" id="tab-description-tab" data-bs-toggle="tab"
                             href="#tab-description" role="tab" aria-controls="tab-description"
-                            aria-selected="true">{{ $product->description }}</a>
+                            aria-selected="true">Description</a>
                     </li>
                     <li class="nav-item" role="presentation">
                         <a class="nav-link nav-link_underscore" id="tab-additional-info-tab" data-bs-toggle="tab"
@@ -218,20 +232,8 @@
                     <div class="tab-pane fade show active" id="tab-description" role="tabpanel"
                         aria-labelledby="tab-description-tab">
                         <div class="product-single__description">
-                            <h3 class="block-title mb-4">Sed do eiusmod tempor incididunt ut labore</h3>
-                            <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                exercitation ullamco
-                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                voluptate
-                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                proident, sunt
-                                in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis
-                                iste natus
-                                error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-                                ab illo
-                                inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                            <h3 class="block-title mb-4">{{$product->name}}</h3>
+                            <p class="content">{{$product->name}}</p>
                             <div class="row">
                                 <div class="col-lg-6">
                                     <h3 class="block-title">Why choose product?</h3>
@@ -284,7 +286,7 @@
                         <div class="product-single__reviews-list">
                             <div class="product-single__reviews-item">
                                 <div class="customer-avatar">
-                                    <img loading="lazy" src="assets/images/avatar.jpg" alt="" />
+                                    <img loading="lazy" src="#" alt="" />
                                 </div>
                                 <div class="customer-review">
                                     <div class="customer-name">
@@ -751,5 +753,5 @@
             </div><!-- /.position-relative -->
 
         </section><!-- /.products-carousel container -->
-    </div>
+    </main>
 @endsection
